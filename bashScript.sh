d55ba9ad53d0db89edb9b26d0ada918888c632fb
Leonardo Acosta <leo@leonardoacosta.dev>
2024-10-30 10:06:17 -0500

    Update

--- bashScript.sh
+++ bashScript.sh
@@ -10,27 +10,52 @@ sudo xcode-select --install
 # Install homebrew taps
 brew tap oven-sh/bun
 
-# Install homebrew packages
-brew install zsh
-brew install --cask google-chrome
-brew install --cask visual-studio-code
-brew install --cask node
-brew install yarn
-brew install bun
-brew install pnpm
-brew install --cask iterm2
-brew install --cask gitkraken
-brew install --cask notion
-brew install --cask cron
-brew install --cask adobe-creative-cloud
-brew install --cask spotify
-brew install --cask discord
-brew install --cask steam
+# Install personal homebrew packages
+brew install \
+  bun \
+  gitkraken \
+  gh \
+  go \
+  iterm2 \
+  minikube \
+  node \
+  nvm \
+  pnpm \
+  yarn \
+  zsh 
 
-# [Refrence](https://github.com/ohmyzsh/ohmyzsh)
-## Don't worry about grabbing the fonts, they install automatically
+brew install --cask \
+  adobe-creative-cloud \
+  azure-data-studio \
+  beekeeper-studio \
+  dbngin \
+  discord \
+  dotnet \
+  fantastical \
+  font-meslo-lg-nerd-font \
+  google-chrome \
+  insomnia \
+  lens \
+  microsoft-edge \
+  microsoft-teams \
+  mongodb-compass \
+  notion \
+  notion-calendar \
+  raycast \
+  signal \
+  slack \
+  spotify \
+  steam \
+  superhuman \
+  telegram \
+  visual-studio-code
+
+# K8S
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"
 
 # Install oh-my-zsh
+# ? [Refrence](https://github.com/ohmyzsh/ohmyzsh)
+# ? Don't worry about grabbing the fonts, they install automatically
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install powerlevel10k
@@ -52,7 +77,10 @@ sed -i '' 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggesti
 #     alias la="colorls -al"
 # fi
 # ```
+
+# Set zsh as the default shell
 source ~/.zshrc
+chsh -s /opt/homebrew/bin/zsh
 
 # Configure powerlevel10k
 p10k configure
